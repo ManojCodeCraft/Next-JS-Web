f0882a4779bdb8557335aefeb1cf1e62d563bd8c
Manoj <aulakagarimanojkumar@gmail.com>
2024-05-22 00:04:43 +0530

    href established

---
+++ next-reviews/app/about/page.jsx
@@ -0,0 +1,10 @@
+export default function AboutPage(){
+    return(
+        <>
+        <h1>About Info</h1>
+        <p>
+           Learning Next.js with an example website
+        </p>
+        </>
+    );
+}
